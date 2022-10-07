5ec9e2ea75837651911207f3101d4df320213118
Madis Liias <madisliias@gmail.com>
2022-10-08 01:04:26 +0300

    linux: install icons

--- build-linux.sh
+++ build-linux.sh
@@ -6,17 +6,35 @@ cross build --target aarch64-unknown-linux-gnu --release
 # Build universal binary
 mkdir -p target/universal-unknown-linux-gnu/release/
 mkdir -p target/universal-unknown-linux-gnu/release/x86_64/
-mkdir -p target/universal-unknown-linux-gnu/release/aarch64/
+mkdir -p target/universal-unknown-linux-gnu/release/icons/
+mkdir -p target/universal-unknown-linux-gnu/release/icons/16x16
+mkdir -p target/universal-unknown-linux-gnu/release/icons/32x32
+mkdir -p target/universal-unknown-linux-gnu/release/icons/64x64
+mkdir -p target/universal-unknown-linux-gnu/release/icons/128x128
+mkdir -p target/universal-unknown-linux-gnu/release/icons/256x256
+mkdir -p target/universal-unknown-linux-gnu/release/icons/512x512
 
 cp "target/x86_64-unknown-linux-gnu/release/browsers" "target/universal-unknown-linux-gnu/release/x86_64/browsers"
 cp "target/aarch64-unknown-linux-gnu/release/browsers" "target/universal-unknown-linux-gnu/release/aarch64/browsers"
 
 cp "extra/linux/dist/install.sh" "target/universal-unknown-linux-gnu/release/install.sh"
 cp "extra/linux/dist/software.Browsers.desktop" "target/universal-unknown-linux-gnu/release/software.Browsers.desktop"
+cp "extra/linux/dist/icons/16x16/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/16x16/software.Browsers.png"
+cp "extra/linux/dist/icons/32x32/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/32x32/software.Browsers.png"
+cp "extra/linux/dist/icons/64x64/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/64x64/software.Browsers.png"
+cp "extra/linux/dist/icons/128x128/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/128x128/software.Browsers.png"
+cp "extra/linux/dist/icons/256x256/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/256x256/software.Browsers.png"
+cp "extra/linux/dist/icons/512x512/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/512x512/software.Browsers.png"
 
 rm -f ./target/universal-unknown-linux-gnu/release/browsers_linux.tar.gz
 
 tar -zcf ./target/universal-unknown-linux-gnu/release/browsers_linux.tar.gz -C ./target/universal-unknown-linux-gnu/release ./x86_64/browsers \
   ./aarch64/browsers \
+  ./icons/16x16/software.Browsers.png \
+  ./icons/32x32/software.Browsers.png \
+  ./icons/64x64/software.Browsers.png \
+  ./icons/128x128/software.Browsers.png \
+  ./icons/256x256/software.Browsers.png \
+  ./icons/512x512/software.Browsers.png \
   ./software.Browsers.desktop \
   ./install.sh
--- extra/linux/dist/install.sh
+++ extra/linux/dist/install.sh
@@ -26,11 +26,24 @@ fi
 # Copy binary to $HOME/.local/bin
 cp "$SRC_BINARY_PATH" "$INSTALL_DIR/browsers"
 
-# Use XDG_DATA_HOME or default to $HOME/.local/share/applications if it's missing
-TARGET_DESKTOP_DIR_PATH="${XDG_DATA_HOME:-$HOME/.local/share/applications}"
+# Installs to /.local/share/icons/hicolor/512x512/apps/software.Browsers.png
+xdg-icon-resource install --novendor --size 16 icons/16x16/software.Browsers.png
+xdg-icon-resource install --novendor --size 32 icons/32x32/software.Browsers.png
+xdg-icon-resource install --novendor --size 64 icons/64x64/software.Browsers.png
+xdg-icon-resource install --novendor --size 128 icons/128x128/software.Browsers.png
+xdg-icon-resource install --novendor --size 256 icons/256x256/software.Browsers.png
+xdg-icon-resource install --novendor --size 512 icons/512x512/software.Browsers.png
+
+# Use XDG_DATA_HOME or default to $HOME/.local/share if it's missing
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+# $HOME/.local/share/applications
+TARGET_ICON_DIR_PATH="$XDG_DATA_HOME/applications"
 
 # Copy .desktop file to $HOME/.local/share/applications/
 desktop-file-install --dir="$TARGET_DESKTOP_DIR_PATH" --rebuild-mime-info-cache "$SOURCE_DESKTOP_FILE_PATH"
 
 # Refresh desktop database
 update-desktop-database "$TARGET_DESKTOP_DIR_PATH"
+
+# Use XDG_DATA_HOME or default to $HOME/.local/share if it's missing
+TARGET_ICON_DIR_PATH="${XDG_DATA_HOME:-$HOME/.local/share}"
