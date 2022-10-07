03f9272048f740c6cb3557463efba3c53e57b27c
Madis Liias <madisliias@gmail.com>
2022-10-07 15:26:18 +0300

    use .tar.gz for linux as it keeps executable bits

--- build-linux.sh
+++ build-linux.sh
@@ -14,11 +14,9 @@ cp "target/aarch64-unknown-linux-gnu/release/browsers" "target/universal-unknown
 cp "extra/linux/dist/install.sh" "target/universal-unknown-linux-gnu/release/install.sh"
 cp "extra/linux/dist/software.Browsers.desktop" "target/universal-unknown-linux-gnu/release/software.Browsers.desktop"
 
-rm -f ./target/universal-unknown-linux-gnu/release/browsers_linux.zip
+rm -f ./target/universal-unknown-linux-gnu/release/browsers_linux.tar.gz
 
-(cd ./target/universal-unknown-linux-gnu/release && zip -r browsers_linux.zip \
-  ./x86_64/browsers \
+tar -zcf ./target/universal-unknown-linux-gnu/release/browsers_linux.tar.gz -C ./target/universal-unknown-linux-gnu/release ./x86_64/browsers \
   ./aarch64/browsers \
   ./software.Browsers.desktop \
-  ./install.sh)
-
+  ./install.sh
