db408df774811c9034bee94fa14d4919aa4867ce
Madis Liias <madisliias@gmail.com>
2023-03-15 21:18:15 +0200

    add .tar.xz archives

--- build-linux.sh
+++ build-linux.sh
@@ -1,55 +1,71 @@
 #!/usr/bin/env bash
 
+# exit when any command fails
+set -e
+
+target_dir='target/universal-unknown-linux-gnu/release'
+
 build() {
   cross build --target x86_64-unknown-linux-gnu --release
   cross build --target aarch64-unknown-linux-gnu --release
 
+  # Clean universal binary
+  rm -rf "${target_dir:?}/"
+
   # Build universal binary
-  mkdir -p target/universal-unknown-linux-gnu/release/
-  mkdir -p target/universal-unknown-linux-gnu/release/x86_64/
-  mkdir -p target/universal-unknown-linux-gnu/release/aarch64/
-  mkdir -p target/universal-unknown-linux-gnu/release/icons/
-  mkdir -p target/universal-unknown-linux-gnu/release/icons/16x16
-  mkdir -p target/universal-unknown-linux-gnu/release/icons/32x32
-  mkdir -p target/universal-unknown-linux-gnu/release/icons/64x64
-  mkdir -p target/universal-unknown-linux-gnu/release/icons/128x128
-  mkdir -p target/universal-unknown-linux-gnu/release/icons/256x256
-  mkdir -p target/universal-unknown-linux-gnu/release/icons/512x512
-  mkdir -p target/universal-unknown-linux-gnu/release/i18n
-  mkdir -p target/universal-unknown-linux-gnu/release/i18n/en-US
+  mkdir -p "$target_dir/"
+  mkdir -p "$target_dir/x86_64/"
+  mkdir -p "$target_dir/aarch64/"
+  mkdir -p "$target_dir/icons/"
+  mkdir -p "$target_dir/icons/16x16"
+  mkdir -p "$target_dir/icons/32x32"
+  mkdir -p "$target_dir/icons/64x64"
+  mkdir -p "$target_dir/icons/128x128"
+  mkdir -p "$target_dir/icons/256x256"
+  mkdir -p "$target_dir/icons/512x512"
+  mkdir -p "$target_dir/i18n"
+  mkdir -p "$target_dir/i18n/en-US"
 
-  cp "target/x86_64-unknown-linux-gnu/release/browsers" "target/universal-unknown-linux-gnu/release/x86_64/browsers"
-  cp "target/aarch64-unknown-linux-gnu/release/browsers" "target/universal-unknown-linux-gnu/release/aarch64/browsers"
+  cp "target/x86_64-unknown-linux-gnu/release/browsers" "$target_dir/x86_64/browsers"
+  cp "target/aarch64-unknown-linux-gnu/release/browsers" "$target_dir/aarch64/browsers"
 
-  cp "extra/linux/dist/install.sh" "target/universal-unknown-linux-gnu/release/install.sh"
-  cp "extra/linux/dist/software.Browsers.desktop" "target/universal-unknown-linux-gnu/release/software.Browsers.desktop"
+  cp "extra/linux/dist/install.sh" "$target_dir/install.sh"
+  cp "extra/linux/dist/software.Browsers.desktop" "$target_dir/software.Browsers.desktop"
 
-  cp "resources/icons/16x16/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/16x16/software.Browsers.png"
-  cp "resources/icons/32x32/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/32x32/software.Browsers.png"
-  cp "resources/icons/64x64/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/64x64/software.Browsers.png"
-  cp "resources/icons/128x128/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/128x128/software.Browsers.png"
-  cp "resources/icons/256x256/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/256x256/software.Browsers.png"
-  cp "resources/icons/512x512/software.Browsers.png" "target/universal-unknown-linux-gnu/release/icons/512x512/software.Browsers.png"
+  cp "resources/icons/16x16/software.Browsers.png" "$target_dir/icons/16x16/software.Browsers.png"
+  cp "resources/icons/32x32/software.Browsers.png" "$target_dir/icons/32x32/software.Browsers.png"
+  cp "resources/icons/64x64/software.Browsers.png" "$target_dir/icons/64x64/software.Browsers.png"
+  cp "resources/icons/128x128/software.Browsers.png" "$target_dir/icons/128x128/software.Browsers.png"
+  cp "resources/icons/256x256/software.Browsers.png" "$target_dir/icons/256x256/software.Browsers.png"
+  cp "resources/icons/512x512/software.Browsers.png" "$target_dir/icons/512x512/software.Browsers.png"
 
-  cp "resources/i18n/en-US/builtin.ftl" "target/universal-unknown-linux-gnu/release/i18n/en-US/builtin.ftl"
+  cp "resources/i18n/en-US/builtin.ftl" "$target_dir/i18n/en-US/builtin.ftl"
 }
 
-make_tar_gz() {
-  rm -f ./target/universal-unknown-linux-gnu/release/browsers_linux.tar.gz
+make_archives() {
+  rm -f "./${target_dir:?}/browsers_linux.tar.gz"
 
-  tar -zcf ./target/universal-unknown-linux-gnu/release/browsers_linux.tar.gz \
-    -C ./target/universal-unknown-linux-gnu/release \
-    ./x86_64/browsers \
-    ./aarch64/browsers \
-    ./icons/16x16/software.Browsers.png \
-    ./icons/32x32/software.Browsers.png \
-    ./icons/64x64/software.Browsers.png \
-    ./icons/128x128/software.Browsers.png \
-    ./icons/256x256/software.Browsers.png \
-    ./icons/512x512/software.Browsers.png \
-    ./software.Browsers.desktop \
-    ./install.sh
+  local filelist=(
+    './x86_64/browsers'
+    './aarch64/browsers'
+    './icons/16x16/software.Browsers.png'
+    './icons/32x32/software.Browsers.png'
+    './icons/64x64/software.Browsers.png'
+    './icons/128x128/software.Browsers.png'
+    './icons/256x256/software.Browsers.png'
+    './icons/512x512/software.Browsers.png'
+    './software.Browsers.desktop'
+    './install.sh'
+  )
+
+  tar -zcf "./$target_dir/browsers_linux.tar.gz" \
+    -C "./$target_dir" \
+    "${filelist[@]}"
+
+  tar -Jcf "./$target_dir/browsers_linux.tar.xz" \
+    -C "./$target_dir" \
+    "${filelist[@]}"
 }
 
 build
-make_tar_gz
+make_archives
--- build-mac.sh
+++ build-mac.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+# exit when any command fails
+set -e
+
 # Load in some secrets
 source .env || true
+
+target_dir='target/universal-apple-darwin/release'
 
 build_binary() {
   # Set minimum macOS version to support older OS versions
   export MACOSX_DEPLOYMENT_TARGET=10.7
 
   # Clean universal binary and app bundle
-  rm -rf target/universal-apple-darwin/release/
+  rm -rf "${target_dir:?}/"
 
   # Build ARM64 binary (also re-creates target/universal-apple-darwin/release/Browsers.app/Contents/Info.plist)
   cargo build --target aarch64-apple-darwin --release
@@ -17,25 +22,25 @@ build_binary() {
   cargo build --target x86_64-apple-darwin --release
 
   # Build universal binary
-  mkdir -p target/universal-apple-darwin/release/
-  lipo -create -output target/universal-apple-darwin/release/Browsers target/x86_64-apple-darwin/release/browsers target/aarch64-apple-darwin/release/browsers
+  mkdir -p "$target_dir/"
+  lipo -create -output "$target_dir/Browsers" target/x86_64-apple-darwin/release/browsers target/aarch64-apple-darwin/release/browsers
 }
 
 build_app_bundle() {
   # Build .app bundle
-  mkdir -p target/universal-apple-darwin/release/Browsers.app/Contents
-  mkdir -p target/universal-apple-darwin/release/Browsers.app/Contents/MacOS
-  mkdir -p target/universal-apple-darwin/release/Browsers.app/Contents/Resources
-  mkdir -p target/universal-apple-darwin/release/Browsers.app/Contents/Resources/i18n/en-US
-  mkdir -p target/universal-apple-darwin/release/Browsers.app/Contents/Resources/icons
-  mkdir -p target/universal-apple-darwin/release/Browsers.app/Contents/Resources/icons/512x512
+  mkdir -p "$target_dir/Browsers.app/Contents"
+  mkdir -p "$target_dir/Browsers.app/Contents/MacOS"
+  mkdir -p "$target_dir/Browsers.app/Contents/Resources"
+  mkdir -p "$target_dir/Browsers.app/Contents/Resources/i18n/en-US"
+  mkdir -p "$target_dir/Browsers.app/Contents/Resources/icons"
+  mkdir -p "$target_dir/Browsers.app/Contents/Resources/icons/512x512"
   # FYI: extra/macos/Info.plist is copied
   #      to target/universal-apple-darwin/release/Browsers.app/Contents/Info.plist
   #      by build.rs (because it also sets version from Cargo.toml)
-  cp extra/macos/icons/Browsers.icns target/universal-apple-darwin/release/Browsers.app/Contents/Resources/Browsers.icns
-  cp resources/icons/512x512/software.Browsers.png target/universal-apple-darwin/release/Browsers.app/Contents/Resources/icons/512x512/software.Browsers.png
-  cp resources/i18n/en-US/builtin.ftl target/universal-apple-darwin/release/Browsers.app/Contents/Resources/i18n/en-US/builtin.ftl
-  cp target/universal-apple-darwin/release/Browsers target/universal-apple-darwin/release/Browsers.app/Contents/MacOS/Browsers
+  cp extra/macos/icons/Browsers.icns "$target_dir/Browsers.app/Contents/Resources/Browsers.icns"
+  cp resources/icons/512x512/software.Browsers.png "$target_dir/Browsers.app/Contents/Resources/icons/512x512/software.Browsers.png"
+  cp resources/i18n/en-US/builtin.ftl "$target_dir/Browsers.app/Contents/Resources/i18n/en-US/builtin.ftl"
+  cp target/universal-apple-darwin/release/Browsers "$target_dir/Browsers.app/Contents/MacOS/Browsers"
 }
 
 sign_app_bundle() {
@@ -44,14 +49,14 @@ sign_app_bundle() {
     --p12-file "$P12_FILE" \
     --p12-password "$P12_PASSWORD" \
     --code-signature-flags runtime \
-    "./target/universal-apple-darwin/release/Browsers.app"
+    "./$target_dir/Browsers.app"
 }
 
 notarize_app_bundle() {
   rcodesign notary-submit \
     --api-key-path "$NOTARY_API_KEY_JSON_FILE" \
     --staple \
-    "./target/universal-apple-darwin/release/Browsers.app"
+    "./$target_dir/Browsers.app"
 }
 
 build_dmg() {
@@ -62,19 +67,23 @@ build_dmg() {
 }
 
 # This is a good format for auto-updating
-make_tar_gz() {
-  rm -f ./target/universal-apple-darwin/release/browsers_mac.tar.gz
+make_archives() {
+  rm -f "./${target_dir:?}/browsers_mac.tar.gz"
 
-  tar -zcf ./target/universal-apple-darwin/release/browsers_mac.tar.gz \
-    -C ./target/universal-apple-darwin/release \
+  # .tar.gz
+  tar -zcf "./$target_dir/browsers_mac.tar.gz" \
+    -C "./$target_dir" \
+    ./Browsers.app
+
+  # .tar.xz
+  tar -Jcf "./$target_dir/browsers_mac.tar.xz" \
+    -C "./$target_dir" \
     ./Browsers.app
 }
-
-make_tar_gz
 
 build_binary
 build_app_bundle
 sign_app_bundle
 notarize_app_bundle
 build_dmg
-make_tar_gz
+make_archives
