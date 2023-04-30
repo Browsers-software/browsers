b6145451fbd97b26c60ff65b44dfc57833612293
Madis Liias <madisliias@gmail.com>
2023-04-30 11:17:42 +0300

    Run only single instance of Browsers
    - Use a lock to see if process already exists
    - Use local socket for interprocess communication, to get url from new instance

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use std::fs;
+use std::sync::mpsc;
+use std::{env, fs};
 
 use rolling_file;
 use rolling_file::{BasicRollingFileAppender, RollingConditionBasic};
+use single_instance::SingleInstance;
 use tracing::info;
 use tracing::metadata::LevelFilter;
 use tracing_subscriber;
 use tracing_subscriber::fmt::time::OffsetTime;
 use tracing_subscriber::fmt::writer::MakeWriterExt;
 
-use browsers::basically_main;
+use browsers::communicate;
 use browsers::paths;
+use browsers::{basically_main, MessageToMain};
 
 fn main() {
     let offset_time = OffsetTime::local_rfc_3339().expect("could not get local offset!");
@@ -38,5 +41,33 @@ fn main() {
     info!("Starting Browsers");
     info!("Logging to {}", log_file_path.display());
 
-    basically_main();
+    let args: Vec<String> = env::args().collect();
+    //info!("{:?}", args);
+
+    let mut url = "".to_string();
+    let url_input_maybe = args.iter().find(|i| i.starts_with("http"));
+    if let Some(url_input) = url_input_maybe {
+        url = url_input.to_string();
+    }
+
+    let show_gui = !args.contains(&"--no-gui".to_string());
+    let force_reload = args.contains(&"--reload".to_string());
+
+    let (main_sender, main_receiver) = mpsc::channel::<MessageToMain>();
+
+    let (is_first_instance, single_instance) =
+        communicate::check_single_instance(url.as_str(), main_sender.clone());
+    if !is_first_instance {
+        info!("Exiting, because another instance is running");
+        return;
+    }
+
+    basically_main(
+        url.as_str(),
+        show_gui,
+        force_reload,
+        main_sender.clone(),
+        main_receiver,
+    );
+    single_instance.is_single(); // dummy as guard
 }
