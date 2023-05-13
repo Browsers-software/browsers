23df98e484b2e39284aff89f5404fefb3bc85eef
Madis Liias <madisliias@gmail.com>
2023-05-13 03:18:06 +0300

    Add windows installer

---
+++ extra/windows/dist/install.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Starting Installation
+
+REM C:\Users\x\AppData\Local\software.Browsers\
+
+REM TODO: would be even more correct to take from registry
+set LocalProgramsDir=%LocalAppData%\Programs
+set ProgramDir=%LocalProgramsDir%\software.Browsers
+
+mkdir "%ProgramDir%"
+copy "aarch64\browsers.exe" "%ProgramDir%\browsers.exe"
+
+mkdir "%ProgramDir%\Resources\icons\512x512"
+copy "icons\512x512\software.Browsers.png" "%ProgramDir%\Resources\icons\512x512\software.Browsers.png"
+
+mkdir "%ProgramDir%\Resources\i18n\en-US"
+copy "i18n\en-US\builtin.ftl" "%ProgramDir%\Resources\i18n\en-US\builtin.ftl"
+
+REM C:\Users\x\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Browsers\Browsers.lnk
+mkdir "%AppData%\Microsoft\Windows\Start Menu\Programs\Browsers"
+copy "startmenu\Browsers.lnk" "%AppData%\Microsoft\Windows\Start Menu\Programs\Browsers\Browsers.lnk"
+
+echo Installation finished!
