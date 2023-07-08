0a24fed9962ee844988f6fdb56f2ff87119ee7fe
Madis Liias <madisliias@gmail.com>
2023-07-08 20:33:05 +0300

    unify

---
+++ extra/linux/linux-build-file-tree.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# exit when any command fails
+set -e
+
+PROJECT_ROOT="../.."
+EXTRA_LINUX_DIST_PATH="./dist/"
+
+PACKAGE_SRC_DIR="$1"
+
+# e.g "target/aarch64-unknown-linux-gnu"
+TARGET_PATH="$2"
+# e.g "$PROJECT_ROOT/target/aarch64-unknown-linux-gnu"
+LINUX_TARGET_DIR="$PROJECT_ROOT/$TARGET_PATH"
+
+create_package_content() {
+  local linux_target_dir="$1"
+  local package_src_dir="$2"
+  local project_resources_dir="$PROJECT_ROOT/resources"
+
+  echo "Creating package content"
+
+  rm -rf "$package_src_dir"
+  mkdir -p "$package_src_dir"
+
+  mkdir -p "$package_src_dir/usr/share/icons/hicolor"
+
+  # /usr/share/icons/hicolor/512x512/apps/software.Browsers.png
+  local sizes=("16x16" "32x32" "64x64" "128x128" "256x256" "512x512")
+
+  for size in "${sizes[@]}"; do
+    mkdir -p "$package_src_dir/usr/share/icons/hicolor/$size/apps"
+    cp "$project_resources_dir/icons/$size/software.Browsers.png" "$package_src_dir/usr/share/icons/hicolor/$size/apps/software.Browsers.png"
+  done
+
+  mkdir -p "$package_src_dir/usr/share/applications"
+
+  local target_install_binary_path="/usr/bin/browsers"
+
+  local template_desktop_file_path="$EXTRA_LINUX_DIST_PATH/software.Browsers.template.desktop"
+
+  mkdir -p "$package_src_dir/usr/share/applications/xfce4/helpers"
+
+  local desktop_file_path="$package_src_dir/usr/share/applications/software.Browsers.desktop"
+  sed "s|€ExecCommand€|$target_install_binary_path %u|g" "$template_desktop_file_path" > "$desktop_file_path"
+
+  local template_xfce4_desktop_file_path="$EXTRA_LINUX_DIST_PATH/xfce4/helpers/software.Browsers.template.desktop"
+  local xfce4_desktop_file_path="$package_src_dir/usr/share/applications/xfce4/helpers/software.Browsers.desktop"
+
+  sed "s|€XFCEBinaries€|browsers;$target_install_binary_path;|g" "$template_xfce4_desktop_file_path" > "$xfce4_desktop_file_path"
+
+  local package_data_dir="$package_src_dir/usr/share/software.Browsers"
+
+  mkdir -p "$package_data_dir"
+
+  mkdir -p "$package_data_dir/resources"
+
+  mkdir -p "$package_data_dir/resources/i18n/en-US"
+  cp "$project_resources_dir/i18n/en-US/builtin.ftl" "$package_data_dir/resources/i18n/en-US/builtin.ftl"
+
+  mkdir -p "$package_data_dir/resources/icons/512x512"
+  cp "$project_resources_dir/icons/512x512/software.Browsers.png" "$package_data_dir/resources/icons/512x512/software.Browsers.png"
+
+  mkdir -p "$package_data_dir/bin"
+
+  # architecture and build specific part (linux_target_dir)
+  # armv7l, aarch64, x86_64
+
+  cp "$linux_target_dir/release/browsers" "$package_data_dir/bin/browsers"
+
+  # Create symlink
+  mkdir -p "$package_src_dir/usr/bin"
+  ln -s "../share/software.Browsers/bin/browsers" "$package_src_dir/usr/bin/browsers"
+}
+
+create_package_content "$LINUX_TARGET_DIR" "$PACKAGE_SRC_DIR"
--- extra/linux/linux-rpm-build.sh
+++ extra/linux/linux-rpm-build.sh
@@ -4,7 +4,6 @@
 set -e
 
 PROJECT_ROOT="../.."
-EXTRA_LINUX_DIST_PATH="./dist/"
 
 RPM_ARCH="$1"
 
@@ -14,76 +13,17 @@ THIS_DIR_ABSOLUTE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 RPM_TOP_DIR_ABSOLUTE="$THIS_DIR_ABSOLUTE/$RPM_TOP_DIR"
 
-RPM_SRC_DIR="${RPM_TOP_DIR}/tree"
-
 # e.g "target/aarch64-unknown-linux-gnu"
 TARGET_PATH="$2"
 # e.g "$PROJECT_ROOT/target/aarch64-unknown-linux-gnu"
 LINUX_TARGET_DIR="$PROJECT_ROOT/$TARGET_PATH"
 
-# TODO: externalize common part for deb and rpm (mostly building the tree)
-create_rpm_content() {
-  local rpm_arch="$1"
+copy_rpm_spec() {
+  local rpm_top_dir="$1"
   local linux_target_dir="$2"
-  local PROJECT_RESOURCES_DIR="$PROJECT_ROOT/resources"
 
-  echo "Creating rpm content"
-
-  rm -rf "$RPM_SRC_DIR"
-  mkdir -p "$RPM_SRC_DIR"
-
-  mkdir -p "$RPM_SRC_DIR/usr/share/icons/hicolor"
-
-  # /usr/share/icons/hicolor/512x512/apps/software.Browsers.png
-  local sizes=("16x16" "32x32" "64x64" "128x128" "256x256" "512x512")
-
-  for size in "${sizes[@]}"; do
-    mkdir -p "$RPM_SRC_DIR/usr/share/icons/hicolor/$size/apps"
-    cp "$PROJECT_RESOURCES_DIR/icons/$size/software.Browsers.png" "$RPM_SRC_DIR/usr/share/icons/hicolor/$size/apps/software.Browsers.png"
-  done
-
-  mkdir -p "$RPM_SRC_DIR/usr/share/applications"
-
-  local TARGET_INSTALL_BINARY_PATH="/usr/bin/browsers"
-
-  local TEMPLATE_DESKTOP_FILE_PATH="$EXTRA_LINUX_DIST_PATH/software.Browsers.template.desktop"
-
-  mkdir -p "$RPM_SRC_DIR/usr/share/applications/xfce4/helpers"
-
-  local DESKTOP_FILE_PATH="$RPM_SRC_DIR/usr/share/applications/software.Browsers.desktop"
-  sed "s|€ExecCommand€|$TARGET_INSTALL_BINARY_PATH %u|g" "$TEMPLATE_DESKTOP_FILE_PATH" > "$DESKTOP_FILE_PATH"
-
-
-  local TEMPLATE_XFCE4_DESKTOP_FILE_PATH="$EXTRA_LINUX_DIST_PATH/xfce4/helpers/software.Browsers.template.desktop"
-  local XFCE4_DESKTOP_FILE_PATH="$RPM_SRC_DIR/usr/share/applications/xfce4/helpers/software.Browsers.desktop"
-
-  sed "s|€XFCEBinaries€|browsers;$TARGET_INSTALL_BINARY_PATH;|g" "$TEMPLATE_XFCE4_DESKTOP_FILE_PATH" > "$XFCE4_DESKTOP_FILE_PATH"
-
-  local RPM_DATA_DIR="$RPM_SRC_DIR/usr/share/software.Browsers"
-
-  mkdir -p "$RPM_DATA_DIR"
-
-  mkdir -p "$RPM_DATA_DIR/resources"
-
-  mkdir -p "$RPM_DATA_DIR/resources/i18n/en-US"
-  cp "$PROJECT_RESOURCES_DIR/i18n/en-US/builtin.ftl" "$RPM_DATA_DIR/resources/i18n/en-US/builtin.ftl"
-
-  mkdir -p "$RPM_DATA_DIR/resources/icons/512x512"
-  cp "$PROJECT_RESOURCES_DIR/icons/512x512/software.Browsers.png" "$RPM_DATA_DIR/resources/icons/512x512/software.Browsers.png"
-
-  mkdir -p "$RPM_DATA_DIR/bin"
-
-  # architecture and build specific part (linux_target_dir)
-  # armv7l, aarch64, x86_64
-
-  cp "$linux_target_dir/release/browsers" "$RPM_DATA_DIR/bin/browsers"
-
-  # Create symlink
-  mkdir -p $RPM_SRC_DIR/usr/bin
-  ln -s "../share/software.Browsers/bin/browsers" "$RPM_SRC_DIR/usr/bin/browsers"
-
-  mkdir -p "$RPM_TOP_DIR/SPECS"
-  cp "$linux_target_dir/meta/rpm_spec/browsers.spec" "$RPM_TOP_DIR/SPECS/browsers.spec"
+  mkdir -p "$rpm_top_dir/SPECS"
+  cp "$linux_target_dir/meta/rpm_spec/browsers.spec" "$rpm_top_dir/SPECS/browsers.spec"
 }
 
 # creates rpm file to e.g "$PROJECT_ROOT/target/aarch64-unknown-linux-gnu/release/browsers_${rpm_arch}.rpm"
@@ -94,7 +34,7 @@ create_rpm() {
   # --build-in-place           run build in current directory
   # --target=CPU-VENDOR-OS     override target platform
   # --target aarch64-fedora-linux
-  #rpmbuild --target --buildroot arm64 -bb "$RPM_SRC_DIR/rpmbuild/SPECS/browsers.spec"
+  #rpmbuild --target --buildroot arm64 -bb "$PACKAGE_SRC_DIR/rpmbuild/SPECS/browsers.spec"
 
   echo "RPM_TOP_DIR_ABSOLUTE is ${RPM_TOP_DIR_ABSOLUTE}"
   rpmbuild --target "${rpm_arch}-linux" --define "_topdir ${RPM_TOP_DIR_ABSOLUTE}" -bb "$RPM_TOP_DIR_ABSOLUTE/SPECS/browsers.spec"
@@ -103,5 +43,7 @@ create_rpm() {
   cp "${RPM_TOP_DIR_ABSOLUTE}/RPMS/$rpm_filename" "$LINUX_TARGET_DIR/release/"
 }
 
-create_rpm_content "$RPM_ARCH" "$LINUX_TARGET_DIR"
+source linux-build-file-tree.sh
+create_package_content "$LINUX_TARGET_DIR" "${RPM_TOP_DIR}/tree"
+copy_rpm_spec "$RPM_TOP_DIR" "$LINUX_TARGET_DIR"
 create_rpm "$RPM_ARCH"
