22c79bdae88449c85487ca78439c9b618c7243cb
Madis Liias <madisliias@gmail.com>
2024-02-21 00:20:00 +0200

    refactor init gtk only if gui is requested

--- src/gui/linux_ui.rs
+++ src/gui/linux_ui.rs
@@ -1,4 +1,4 @@
 pub fn init_gtk() {
     // must be initialized in main thread (because of gtk requirements)
-    let _result = gtk::init().expect("Could not initialize gtk");
+    gtk::init().expect("Could not initialize gtk");
 }
