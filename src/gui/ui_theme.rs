d7d9403ad9e74a6756d1943dac825684a9640cf8
Madis Liias <madisliias@gmail.com>
2024-06-28 00:27:26 +0300

    wip

--- src/gui/ui_theme.rs
+++ src/gui/ui_theme.rs
@@ -60,9 +60,8 @@ pub fn setup_theme(env: &mut Env, ui_theme: UITheme) {
 }
 
 fn set_env_to_theme(env: &mut Env, theme: Theme) {
-    //env.adding()
     env.set(ENV_WINDOW_BACKGROUND_COLOR, theme.window_background_color);
-    env.set(ENV_WINDOW_BORDER_COLOR, theme.window_background_color);
+    env.set(ENV_WINDOW_BORDER_COLOR, theme.window_border_color);
 }
 
 struct Theme {
@@ -71,8 +70,6 @@ struct Theme {
 }
 
 struct Palette {}
-
-// .adding(BACKGROUND_DARK, Color::rgb8(0x31, 0x31, 0x31))
 
 /*
 env.adding(WINDOW_BACKGROUND_COLOR, Color::rgb8(0x29, 0x29, 0x29))
@@ -140,113 +137,3 @@ env.adding(WINDOW_BACKGROUND_COLOR, Color::rgb8(0x29, 0x29, 0x29))
                .with_size(15.0),
        )
  **/
-/*
-pub fn setup(env: &mut Env, state: &UIState) {
-    match state.config.theme {
-        Theme::Light => setup_light_theme(env),
-        Theme::Dark => setup_dark_theme(env),
-    };
-
-    env.set(WINDOW_BACKGROUND_COLOR, env.get(GREY_700));
-    env.set(TEXT_COLOR, env.get(GREY_100));
-    env.set(ICON_COLOR, env.get(GREY_400));
-    env.set(PLACEHOLDER_COLOR, env.get(GREY_400));
-    env.set(PRIMARY_LIGHT, env.get(BLUE_100));
-    env.set(PRIMARY_DARK, env.get(BLUE_200));
-
-    env.set(BACKGROUND_LIGHT, env.get(GREY_700));
-    env.set(BACKGROUND_DARK, env.get(GREY_600));
-    env.set(FOREGROUND_LIGHT, env.get(GREY_100));
-    env.set(FOREGROUND_DARK, env.get(GREY_000));
-
-    match state.config.theme {
-        Theme::Light => {
-            env.set(BUTTON_LIGHT, env.get(GREY_700));
-            env.set(BUTTON_DARK, env.get(GREY_600));
-        }
-        Theme::Dark => {
-            env.set(BUTTON_LIGHT, env.get(GREY_600));
-            env.set(BUTTON_DARK, env.get(GREY_700));
-        }
-    }
-
-    env.set(BORDER_LIGHT, env.get(GREY_400));
-    env.set(BORDER_DARK, env.get(GREY_500));
-
-    env.set(SELECTED_TEXT_BACKGROUND_COLOR, env.get(BLUE_200));
-    env.set(SELECTION_TEXT_COLOR, env.get(GREY_700));
-
-    env.set(CURSOR_COLOR, env.get(GREY_000));
-
-    env.set(PROGRESS_BAR_RADIUS, 4.0);
-    env.set(BUTTON_BORDER_RADIUS, 4.0);
-    env.set(BUTTON_BORDER_WIDTH, 1.0);
-
-    env.set(
-        UI_FONT,
-        FontDescriptor::new(FontFamily::SYSTEM_UI).with_size(13.0),
-    );
-    env.set(
-        UI_FONT_MEDIUM,
-        FontDescriptor::new(FontFamily::SYSTEM_UI)
-            .with_size(13.0)
-            .with_weight(FontWeight::MEDIUM),
-    );
-    env.set(
-        UI_FONT_MONO,
-        FontDescriptor::new(FontFamily::MONOSPACE).with_size(13.0),
-    );
-    env.set(TEXT_SIZE_SMALL, 11.0);
-    env.set(TEXT_SIZE_NORMAL, 13.0);
-    env.set(TEXT_SIZE_LARGE, 16.0);
-
-    env.set(BASIC_WIDGET_HEIGHT, 16.0);
-    env.set(WIDE_WIDGET_WIDTH, grid(12.0));
-    env.set(BORDERED_WIDGET_HEIGHT, grid(4.0));
-
-    env.set(TEXTBOX_BORDER_RADIUS, 4.0);
-    env.set(TEXTBOX_BORDER_WIDTH, 1.0);
-    env.set(TEXTBOX_INSETS, Insets::uniform_xy(grid(1.2), grid(1.0)));
-
-    env.set(SCROLLBAR_COLOR, env.get(GREY_300));
-    env.set(SCROLLBAR_BORDER_COLOR, env.get(GREY_300));
-    env.set(SCROLLBAR_MAX_OPACITY, 0.8);
-    env.set(SCROLLBAR_FADE_DELAY, 1500u64);
-    env.set(SCROLLBAR_WIDTH, 6.0);
-    env.set(SCROLLBAR_PAD, 2.0);
-    env.set(SCROLLBAR_RADIUS, 5.0);
-    env.set(SCROLLBAR_EDGE_WIDTH, 1.0);
-
-    env.set(WIDGET_PADDING_VERTICAL, grid(0.5));
-    env.set(WIDGET_PADDING_HORIZONTAL, grid(1.0));
-    env.set(WIDGET_CONTROL_COMPONENT_PADDING, grid(1.0));
-
-    env.set(MENU_BUTTON_BG_ACTIVE, env.get(GREY_500));
-    env.set(MENU_BUTTON_BG_INACTIVE, env.get(GREY_600));
-    env.set(MENU_BUTTON_FG_ACTIVE, env.get(GREY_000));
-    env.set(MENU_BUTTON_FG_INACTIVE, env.get(GREY_100));
-}
-
-fn setup_light_theme(env: &mut Env) {
-    setup_dark_theme(env);
-}
-
-fn setup_dark_theme(env: &mut Env) {
-    env.set(GREY_000, Color::grey8(0xff));
-    env.set(GREY_100, Color::grey8(0xf2));
-    env.set(GREY_200, Color::grey8(0xe0));
-    env.set(GREY_300, Color::grey8(0xbd));
-    env.set(GREY_400, Color::grey8(0x82));
-    env.set(GREY_500, Color::grey8(0x4f));
-    env.set(GREY_600, Color::grey8(0x33));
-    env.set(GREY_700, Color::grey8(0x28));
-    env.set(BLUE_100, Color::rgb8(0x00, 0x8d, 0xdd));
-    env.set(BLUE_200, Color::rgb8(0x5c, 0xc4, 0xff));
-
-    env.set(RED, Color::rgba8(0xEB, 0x57, 0x57, 0xFF));
-
-    env.set(LINK_HOT_COLOR, Color::rgba(1.0, 1.0, 1.0, 0.05));
-    env.set(LINK_ACTIVE_COLOR, Color::rgba(1.0, 1.0, 1.0, 0.025));
-    env.set(LINK_COLD_COLOR, Color::rgba(1.0, 1.0, 1.0, 0.0));
-}
-*/
