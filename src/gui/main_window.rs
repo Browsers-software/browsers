a59c40230103dddfe23f9a17d4359dea3e312718
Madis Liias <madis@qminder.com>
2026-08-27 00:11:20 +0300

    fix eyeglasses emoji size when using a font which does not have the emoji

--- src/gui/main_window.rs
+++ src/gui/main_window.rs
@@ -324,14 +324,13 @@ pub(crate) fn calculate_window_position(
 }
 
 fn create_browser_label() -> impl Widget<((bool, UISettings), UIBrowser)> {
-    let browser_label = Label::dynamic(|((_, _), item): &((bool, UISettings), UIBrowser), _env| {
-        item.browser_name.clone()
-    })
-    .with_font(MainWindowTheme::ENV_BROWSER_LABEL_FONT_FAMILY)
-    .with_text_size(MainWindowTheme::ENV_BROWSER_LABEL_SIZE)
-    .with_line_break_mode(LineBreaking::Clip)
-    .with_text_alignment(TextAlignment::Start)
-    .with_text_color(MainWindowTheme::ENV_BROWSER_LABEL_COLOR);
+    let browser_label = Label::dynamic(|item: &UIBrowser, _env| item.browser_name.clone())
+        .with_font(MainWindowTheme::ENV_BROWSER_LABEL_FONT_FAMILY)
+        .with_text_size(MainWindowTheme::ENV_BROWSER_LABEL_SIZE)
+        .with_line_break_mode(LineBreaking::Clip)
+        .with_text_alignment(TextAlignment::Start)
+        .with_text_color(MainWindowTheme::ENV_BROWSER_LABEL_COLOR)
+        .lens(BrowserLens);
 
     // fixed size so that fonts which lack the emoji don't mess up the text label size
     let incognito_indicator = Either::new(
